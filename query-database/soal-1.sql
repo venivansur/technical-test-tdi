ca2eaaf0f8322320abbfb1702b641c4c09ef58de
unknown <vansurya.01@gmail.com>
2025-05-03 17:12:06 +0700

    feat(query): Add all queries for soal 3 (SQL task)

---
+++ query-database/soal-1.sql
@@ -0,0 +1,9 @@
+SELECT 
+  o.id AS order_id,
+  o.customer_name,
+  o.order_date,
+  SUM(oi.quantity) AS total_items,
+  SUM(oi.quantity * oi.price) AS total_price
+FROM orders o
+JOIN order_items oi ON o.id = oi.order_id
+GROUP BY o.id, o.customer_name, o.order_date;
