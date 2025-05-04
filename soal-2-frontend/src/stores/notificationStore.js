3f6e87d1c72dca9ad2372b44ffa2dba15ae7e942
unknown <vansurya.01@gmail.com>
2025-05-04 09:48:02 +0700

    fix: fxing branch

--- soal-2-frontend/src/stores/notificationStore.js
+++
@@ -1,9 +0,0 @@
-export default {
-    state: { message: null },
-    mutations: {
-      setMessage(state, msg) {
-        state.message = msg;
-        setTimeout(() => state.message = null, 3000);
-      }
-    }
-  }
