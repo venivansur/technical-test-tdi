ebd0bbc900869a46fd5d74d6341eb2857e8a7806
unknown <vansurya.01@gmail.com>
2025-05-04 09:54:48 +0700

    fix: fxing branch

--- query-database/soal-3.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-  p.id AS product_id,
-  p.name AS product_name,
-  SUM(oi.quantity) AS total_quantity
-FROM products p
-JOIN order_items oi ON p.id = oi.product_id
-GROUP BY p.id, p.name
-ORDER BY total_quantity DESC
-LIMIT 1;
