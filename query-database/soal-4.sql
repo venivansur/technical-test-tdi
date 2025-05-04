da59e608d9f66e64ac6568bfe31d478a67c33aac
unknown <vansurya.01@gmail.com>
2025-05-04 09:29:42 +0700

    fix: fixing branch

--- query-database/soal-4.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  p.id AS product_id,
-  p.name AS product_name,
-  p.stock + IFNULL(SUM(oi.quantity), 0) AS initial_stock,
-  p.stock AS remaining_stock
-FROM products p
-LEFT JOIN order_items oi ON p.id = oi.product_id
-GROUP BY p.id, p.name, p.stock;
--- query-database/soal-5.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-  o.id AS order_id,
-  o.customer_name,
-  o.order_date,
-  SUM(oi.quantity * oi.price) AS total_price
-FROM orders o
-JOIN order_items oi ON o.id = oi.order_id
-WHERE MONTH(o.order_date) = 11 AND YEAR(o.order_date) = 2024
-GROUP BY o.id, o.customer_name, o.order_date;
